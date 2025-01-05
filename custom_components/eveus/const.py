73c0d968e05e952ba250ac25110a6ed6494007ec
ABovsh <143256790+ABovsh@users.noreply.github.com>
2025-01-05 23:49:22 +0200

    Update const.py

--- custom_components/eveus/const.py
+++ custom_components/eveus/const.py
@@ -3,6 +3,5 @@ from datetime import timedelta
 import logging
 
 DOMAIN = "eveus"
-SCAN_INTERVAL = timedelta(seconds=30)
+SCAN_INTERVAL = timedelta(seconds=10)  # Shorter interval for testing
 LOGGER = logging.getLogger(__package__)
-LOGGER.setLevel(logging.DEBUG)
