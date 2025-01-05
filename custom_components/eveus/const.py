3fcbbd42f6645903ce0c5523694976d079f8489b
ABovsh <143256790+ABovsh@users.noreply.github.com>
2025-01-05 21:50:42 +0200

    Update const.py

--- custom_components/eveus/const.py
+++ custom_components/eveus/const.py
@@ -1,4 +1,9 @@
+"""Constants for the Eveus integration."""
 from datetime import timedelta
+import logging
 
 DOMAIN = "eveus"
 SCAN_INTERVAL = timedelta(seconds=60)
+
+# Logger
+LOGGER = logging.getLogger(__package__)
