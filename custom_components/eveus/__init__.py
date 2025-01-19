ddfae8a67b3be43093f8ca075d9c99a751a9c65c
ABovsh <143256790+ABovsh@users.noreply.github.com>
2025-01-20 00:04:41 +0200

    Update __init__.py

--- custom_components/eveus/__init__.py
+++ custom_components/eveus/__init__.py
@@ -110,7 +110,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
                 config_entry_id=entry.entry_id,
                 identifiers={(DOMAIN, entry.data[CONF_HOST])},
                 manufacturer="Eveus",
-                name=f"Current range: {session_manager.capabilities['min_current']}-{session_manager.capabilities['max_current']}A",
+                name=f"{session_manager.capabilities['min_current']}-{session_manager.capabilities['max_current']}A",
                 model="Eveus",
                 sw_version=session_manager.firmware_version,
                 configuration_url=f"http://{entry.data[CONF_HOST]}",
