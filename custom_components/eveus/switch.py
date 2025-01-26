f14b876db29a77f119cdc3fc8f526e83da2b4469
ABovsh <143256790+ABovsh@users.noreply.github.com>
2025-01-26 21:31:02 +0200

    Update switch.py

--- custom_components/eveus/switch.py
+++ custom_components/eveus/switch.py
@@ -68,7 +68,9 @@ class BaseEveusSwitch(SwitchEntity):
            "identifiers": {(DOMAIN, self._host)},
            "name": "Eveus EV Charger",
            "manufacturer": "Eveus",
-           "model": f"Eveus ({self._host})"
+           "model": f"({self._host})",
+           "sw_version": self._session.data.get("verFWMain", "Unknown") if self._session else "Unknown",
+           "hw_version": self._session.data.get("serialNum", "Unknown") if self._session else "Unknown"
        }
 
    async def _get_session(self) -> aiohttp.ClientSession:
