26a6e63bdac9a06829bb903ce76b662a54e0d9db
ABovsh <143256790+ABovsh@users.noreply.github.com>
2025-02-16 13:30:04 +0200

    Update basic_sensors.py

--- custom_components/eveus/basic_sensors.py
+++ custom_components/eveus/basic_sensors.py
@@ -1,4 +1,4 @@
-"""Support for Eveus basic measurement sensors."""
+"""Support for basic Eveus sensors."""
 from __future__ import annotations
 
 import logging
@@ -9,8 +9,8 @@ from homeassistant.components.sensor import (
     SensorStateClass,
 )
 from homeassistant.const import (
-    UnitOfElectricPotential,
     UnitOfElectricCurrent,
+    UnitOfElectricPotential,
     UnitOfPower,
     UnitOfEnergy,
     UnitOfTime,
@@ -21,93 +21,162 @@ from .utils import get_safe_value, format_duration
 
 _LOGGER = logging.getLogger(__name__)
 
-class EveusBasicMeasurementSensor(EveusSensorBase):
-    """Base class for basic measurement sensors."""
-    
-    DATA_KEY: str = None
-    _attr_suggested_display_precision = 1
-    
-    @property
-    def native_value(self) -> float | None:
-        """Return sensor value."""
-        if not self.DATA_KEY:
-            raise NotImplementedError("DATA_KEY must be defined in child class")
-        return get_safe_value(self._updater.data, self.DATA_KEY)
+class EveusVoltageSensor(EveusSensorBase):
+    """Voltage sensor."""
 
-class EveusVoltageSensor(EveusBasicMeasurementSensor):
-    """Voltage measurement sensor."""
-    
     ENTITY_NAME = "Voltage"
-    DATA_KEY = "voltMeas1"
     _attr_device_class = SensorDeviceClass.VOLTAGE
     _attr_native_unit_of_measurement = UnitOfElectricPotential.VOLT
     _attr_state_class = SensorStateClass.MEASUREMENT
-    _attr_icon = "mdi:lightning-bolt"
-    _attr_suggested_display_precision = 0
+    _attr_icon = "mdi:voltage-ac"
+    _attr_suggested_display_precision = 1
 
-class EveusCurrentSensor(EveusBasicMeasurementSensor):
-    """Current measurement sensor."""
-    
+    @property
+    def native_value(self) -> float | None:
+        """Return voltage."""
+        try:
+            value = get_safe_value(self._updater.data, "voltMeas1")
+            if value is None:
+                return None
+            return round(value, 1)
+        except Exception as err:
+            _LOGGER.error("Error getting voltage: %s", err)
+            return None
+
+class EveusCurrentSensor(EveusSensorBase):
+    """Current sensor."""
+
     ENTITY_NAME = "Current"
-    DATA_KEY = "curMeas1"
     _attr_device_class = SensorDeviceClass.CURRENT
     _attr_native_unit_of_measurement = UnitOfElectricCurrent.AMPERE
     _attr_state_class = SensorStateClass.MEASUREMENT
     _attr_icon = "mdi:current-ac"
+    _attr_suggested_display_precision = 1
 
-class EveusPowerSensor(EveusBasicMeasurementSensor):
-    """Power measurement sensor."""
-    
+    @property
+    def native_value(self) -> float | None:
+        """Return current."""
+        try:
+            value = get_safe_value(self._updater.data, "curMeas1")
+            if value is None:
+                return None
+            return round(value, 1)
+        except Exception as err:
+            _LOGGER.error("Error getting current: %s", err)
+            return None
+
+class EveusPowerSensor(EveusSensorBase):
+    """Power sensor."""
+
     ENTITY_NAME = "Power"
-    DATA_KEY = "powerMeas"
     _attr_device_class = SensorDeviceClass.POWER
     _attr_native_unit_of_measurement = UnitOfPower.WATT
     _attr_state_class = SensorStateClass.MEASUREMENT
     _attr_icon = "mdi:flash"
-    _attr_suggested_display_precision = 0
+    _attr_suggested_display_precision = 1
 
-class EveusCurrentSetSensor(EveusBasicMeasurementSensor):
+    @property
+    def native_value(self) -> float | None:
+        """Return power."""
+        try:
+            value = get_safe_value(self._updater.data, "powerMeas")
+            if value is None:
+                return None
+            return round(value, 1)
+        except Exception as err:
+            _LOGGER.error("Error getting power: %s", err)
+            return None
+
+class EveusCurrentSetSensor(EveusSensorBase):
     """Current set sensor."""
 
     ENTITY_NAME = "Current Set"
-    DATA_KEY = "currentSet"
     _attr_device_class = SensorDeviceClass.CURRENT
     _attr_native_unit_of_measurement = UnitOfElectricCurrent.AMPERE
     _attr_state_class = SensorStateClass.MEASUREMENT
     _attr_icon = "mdi:current-ac"
     _attr_suggested_display_precision = 0
 
+    @property
+    def native_value(self) -> float | None:
+        """Return current set."""
+        try:
+            value = get_safe_value(self._updater.data, "currentSet")
+            if value is None:
+                return None
+            return round(value, 0)
+        except Exception as err:
+            _LOGGER.error("Error getting current set: %s", err)
+            return None
+
 class EveusSessionTimeSensor(EveusSensorBase):
     """Session time sensor."""
-    
+
     ENTITY_NAME = "Session Time"
     _attr_icon = "mdi:timer"
-    
-    @property
-    def native_value(self) -> str:
-        """Return formatted session time."""
-        seconds = get_safe_value(self._updater.data, "sessionTime", int, 0)
-        return format_duration(seconds)
+    _attr_device_class = SensorDeviceClass.DURATION
+    _attr_native_unit_of_measurement = UnitOfTime.SECONDS
 
-class EveusEnergySensorBase(EveusBasicMeasurementSensor):
-    """Base energy sensor."""
-    
+    @property
+    def native_value(self) -> int | None:
+        """Return session time in seconds."""
+        try:
+            return get_safe_value(self._updater.data, "sessionTime", int)
+        except Exception as err:
+            _LOGGER.error("Error getting session time: %s", err)
+            return None
+
+    @property
+    def extra_state_attributes(self) -> dict[str, Any]:
+        """Return formatted duration."""
+        try:
+            seconds = get_safe_value(self._updater.data, "sessionTime", int)
+            if seconds is not None:
+                return {"formatted_duration": format_duration(seconds)}
+        except Exception as err:
+            _LOGGER.error("Error formatting duration: %s", err)
+        return {}
+
+class EveusSessionEnergySensor(EveusSensorBase):
+    """Session energy sensor."""
+
+    ENTITY_NAME = "Session Energy"
     _attr_device_class = SensorDeviceClass.ENERGY
     _attr_native_unit_of_measurement = UnitOfEnergy.KILO_WATT_HOUR
-    _attr_icon = "mdi:battery-charging"
-    _attr_suggested_display_precision = 1
-
-class EveusSessionEnergySensor(EveusEnergySensorBase):
-    """Session energy sensor."""
-    
-    ENTITY_NAME = "Session Energy"
-    DATA_KEY = "sessionEnergy"
     _attr_state_class = SensorStateClass.TOTAL
+    _attr_icon = "mdi:transmission-tower-export"
+    _attr_suggested_display_precision = 2
 
-class EveusTotalEnergySensor(EveusEnergySensorBase):
+    @property
+    def native_value(self) -> float | None:
+        """Return session energy."""
+        try:
+            value = get_safe_value(self._updater.data, "sessionEnergy")
+            if value is None:
+                return None
+            return round(value, 2)
+        except Exception as err:
+            _LOGGER.error("Error getting session energy: %s", err)
+            return None
+
+class EveusTotalEnergySensor(EveusSensorBase):
     """Total energy sensor."""
-    
+
     ENTITY_NAME = "Total Energy"
-    DATA_KEY = "totalEnergy"
+    _attr_device_class = SensorDeviceClass.ENERGY
+    _attr_native_unit_of_measurement = UnitOfEnergy.KILO_WATT_HOUR
     _attr_state_class = SensorStateClass.TOTAL_INCREASING
-    _attr_icon = "mdi:battery-charging-100"
+    _attr_icon = "mdi:transmission-tower"
+    _attr_suggested_display_precision = 2
+
+    @property
+    def native_value(self) -> float | None:
+        """Return total energy."""
+        try:
+            value = get_safe_value(self._updater.data, "totalEnergy")
+            if value is None:
+                return None
+            return round(value, 2)
+        except Exception as err:
+            _LOGGER.error("Error getting total energy: %s", err)
+            return None
