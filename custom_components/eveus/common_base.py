58ec5d59c8d4d3b0e382e18c011a169e4247a132
ABovsh <143256790+ABovsh@users.noreply.github.com>
2025-07-27 14:15:16 +0300

    Update common_base.py

--- custom_components/eveus/common_base.py
+++ custom_components/eveus/common_base.py
@@ -7,7 +7,7 @@ from homeassistant.helpers.restore_state import RestoreEntity
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.helpers.entity import EntityCategory
 
-from .utils import get_device_info
+from .utils import get_device_info, get_device_suffix
 
 class BaseEveusEntity(RestoreEntity, Entity):
     """Base implementation for Eveus entities."""
@@ -16,17 +16,22 @@ class BaseEveusEntity(RestoreEntity, Entity):
     _attr_has_entity_name = True
     _attr_should_poll = False
 
-    def __init__(self, updater) -> None:
+    def __init__(self, updater, device_number: int = 1) -> None:
         """Initialize the entity."""
         super().__init__()
         self._updater = updater
+        self._device_number = device_number
         self._updater.register_entity(self)
 
         if self.ENTITY_NAME is None:
             raise NotImplementedError("ENTITY_NAME must be defined in child class")
 
         self._attr_name = self.ENTITY_NAME
-        self._attr_unique_id = f"eveus_{self.ENTITY_NAME.lower().replace(' ', '_')}"
+        
+        # Generate unique ID with device suffix for multi-device support
+        device_suffix = get_device_suffix(device_number)
+        entity_key = self.ENTITY_NAME.lower().replace(' ', '_')
+        self._attr_unique_id = f"eveus{device_suffix}_{entity_key}"
 
     @property
     def available(self) -> bool:
@@ -36,7 +41,7 @@ class BaseEveusEntity(RestoreEntity, Entity):
     @property
     def device_info(self) -> dict[str, Any]:
         """Return device information."""
-        return get_device_info(self._updater.host, self._updater.data)
+        return get_device_info(self._updater.host, self._updater.data, self._device_number)
 
     async def async_added_to_hass(self) -> None:
         """Handle entity which will be added."""
@@ -63,9 +68,9 @@ class BaseEveusEntity(RestoreEntity, Entity):
 class EveusSensorBase(BaseEveusEntity, SensorEntity):
     """Base sensor entity for Eveus."""
     
-    def __init__(self, updater: "EveusUpdater") -> None:
+    def __init__(self, updater: "EveusUpdater", device_number: int = 1) -> None:
         """Initialize the sensor."""
-        super().__init__(updater)
+        super().__init__(updater, device_number)
         self._attr_native_value = None
 
     @property
